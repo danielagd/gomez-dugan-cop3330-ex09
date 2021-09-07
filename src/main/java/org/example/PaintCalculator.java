22c888a354d4dfb274c5dab7c6dfc46691a2f388
Daniela <danigomezdugan@gmail.com>
2021-09-07 17:36:17 -0400

    Initial commit

---
+++ src/main/java/org/example/PaintCalculator.java
@@ -0,0 +1,41 @@
+package org.example;
+import java.util.Scanner;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Daniela Gomez-Dugan
+ */
+public class PaintCalculator
+{
+    public static void main( String[] args )
+    {
+        final int one_gallon = 350;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("What is the length of the ceiling in feet? ");
+        double length = input.nextDouble();
+        while (length < 0) {
+            System.out.print("Negative values are invalid. Please enter a positive length: ");
+            length = input.nextDouble();
+        }
+
+        System.out.print("What is the width of the ceiling in feet? ");
+        double width = input.nextDouble();
+        while (width < 0) {
+            System.out.print("Negative values are invalid. Please enter a positive width: ");
+            width = input.nextDouble();
+        }
+
+        double area = length * width;
+        int num_gallons = (int) (area / 350);
+
+        int leftover_area = (int) (area % 350);
+        if (leftover_area > 0)
+            num_gallons++;
+
+        if (num_gallons == 1)
+            System.out.println("You will need to purchase " + num_gallons + " gallon of paint to cover " + area + " square feet.");
+        else
+            System.out.println("You will need to purchase " + num_gallons + " gallons of paint to cover " + area + " square feet.");
+    }
+}
