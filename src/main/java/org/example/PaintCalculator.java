2ba9a15da1552e55b0cb6bfae754dfa82b78e9bc
Daniela <danigomezdugan@gmail.com>
2021-09-09 21:48:34 -0400

    updating file

--- src/main/java/org/example/PaintCalculator.java
+++ src/main/java/org/example/PaintCalculator.java
@@ -9,11 +9,15 @@ public class PaintCalculator
 {
     public static void main( String[] args )
     {
+        // Program to calculate the amount of gallons to paint a ceiling
+
         final int one_gallon = 350;
         Scanner input = new Scanner(System.in);
 
         System.out.print("What is the length of the ceiling in feet? ");
         double length = input.nextDouble();
+
+        // Checking for negative values
         while (length < 0) {
             System.out.print("Negative values are invalid. Please enter a positive length: ");
             length = input.nextDouble();
@@ -21,6 +25,8 @@ public class PaintCalculator
 
         System.out.print("What is the width of the ceiling in feet? ");
         double width = input.nextDouble();
+
+        // Checking for negative values
         while (width < 0) {
             System.out.print("Negative values are invalid. Please enter a positive width: ");
             width = input.nextDouble();
@@ -33,6 +39,7 @@ public class PaintCalculator
         if (leftover_area > 0)
             num_gallons++;
 
+        // Changing of plural to singular depending on calculation
         if (num_gallons == 1)
             System.out.println("You will need to purchase " + num_gallons + " gallon of paint to cover " + area + " square feet.");
         else
